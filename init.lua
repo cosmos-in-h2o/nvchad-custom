46aa81074866d5d2dc8bc781df5ed3c60bb5079a
makise-curisu <dream77070@163.com>
2023-11-19 13:20:47 +0800

    update

--- init.lua
+++ init.lua
@@ -1,7 +1,27 @@
--- local autocmd = vim.api.nvim_create_autocmd
+-------------------------------------- custom ------------------------------------------
+-- copy高亮
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+    pattern = { "*" },
+    callback = function()
+        vim.highlight.on_yank {
+            timeout = 300,
+        }
+    end,
+})
 
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+-- 折叠相关
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldenable = false
+vim.o.foldlevel = 99
+
+--相对行号
+vim.opt.relativenumber = true
+
+-- Indenting
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+-- 字体
+vim.o.guifont = "JetBrainsMono Nerd Font:h20"
